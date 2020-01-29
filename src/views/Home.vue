dbfcca97779d9e55252e043d5738a35deed4bf14
Donastor, Sebastian <s.donastor@motorcredito.com.do>
2020-01-29 15:39:38 -0800

    adding logo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home container">
+    <img alt="Vue logo" class="img-fluid" src="../assets/PokeFinder2.png">
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
